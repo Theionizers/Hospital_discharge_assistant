2e191d2476788bf0c114cfc4b370ce8ec91c0166
the ionizers <pwallah69@gmail.com>
2026-07-28 16:35:48 +0530

    added all nodes in graph

--- Ai/Graph/graph.py
+++ Ai/Graph/graph.py
@@ -11,21 +11,31 @@ from Ai.Nodes.patient import patient
 from Ai.Nodes.medicine_timetable import medication_timetable
 from Ai.Nodes.tests import tests
 from Ai.Nodes.exercise_restriction import exercise_restrictions
+
 from Ai.State.Graph_state import Hopitaldata
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.sqlite import SqliteSaver
 
 builder = StateGraph(Hopitaldata)
 
+# -------------------- Nodes --------------------
+
 builder.add_node("start", start_node)
 builder.add_node("router", start_router)
+
 builder.add_node("diet", diet_plan)
 builder.add_node("warning", warning_signs)
 builder.add_node("general", general)
 builder.add_node("medication", medication)
 builder.add_node("life_style_advise", life_style)
 builder.add_node("simple_explanation", simple_explanation)
-builder.add_node("simple_explanation", simple_explanation)
+builder.add_node("contact_information", contact_information)
+builder.add_node("patient", patient)
+builder.add_node("medicine_timetable", medication_timetable)
+builder.add_node("tests", tests)
+builder.add_node("exercise_restrictions", exercise_restrictions)
+
+# -------------------- Edges --------------------
 
 builder.add_edge(START, "start")
 builder.add_edge("start", "router")
@@ -36,18 +46,34 @@ builder.add_conditional_edges(
     {
         "diet_plan": "diet",
         "medications": "medication",
+        "medicine_timetable": "medicine_timetable",
+        "exercise_restrictions": "exercise_restrictions",
         "warning_signs": "warning",
+        "patient": "patient",
+        "follow_up": "general",                 # Change when you create a follow_up node
+        "tests": "tests",
+        "lifestyle_advice": "life_style_advise",
+        "contact_information": "contact_information",
+        "simple_explanation": "simple_explanation",
         "general": "general",
-        "lifestyle_advice":"life_style_advise",
-        "simple_explanation":"simple_explanation"
     },
 )
 
+# -------------------- End Edges --------------------
+
 builder.add_edge("diet", END)
 builder.add_edge("medication", END)
+builder.add_edge("medicine_timetable", END)
+builder.add_edge("exercise_restrictions", END)
 builder.add_edge("warning", END)
+builder.add_edge("patient", END)
+builder.add_edge("tests", END)
+builder.add_edge("life_style_advise", END)
+builder.add_edge("contact_information", END)
+builder.add_edge("simple_explanation", END)
 builder.add_edge("general", END)
 
+# -------------------- Compile --------------------
 
 with SqliteSaver.from_conn_string("chat_memory.db") as memory:
     graph = builder.compile(checkpointer=memory)
@@ -77,4 +103,4 @@ with SqliteSaver.from_conn_string("chat_memory.db") as memory:
 
         print("\nAssistant:")
         print(result["response"])
-        print("-" * 50)
+        print("-" * 60)
